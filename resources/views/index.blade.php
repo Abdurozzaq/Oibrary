06aa9adffb628ff3a7da5adda06ec7b5e94f7af4
Abdurozzaq Nurul Hadi <Abdurozzaq@outlook.com>
2020-08-31 00:01:37 +0700

    Modified Name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@500&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
-        <title>Laravel Vuetify</title>
+        <title>Aplikasi Perpustakaan</title>
+        <link rel="icon" href="/statics/logo-sekolah.png" type="image/gif" sizes="16x16">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
     </head>
     <body>
