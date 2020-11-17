e87d0c13a323bd835a2f970bd21db6e29bc328c4
Abdurozzaq Nurul Hadi <Abdurozzaq@outlook.com>
2020-11-18 00:52:01 +0700

    BUG in route again

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
 }
