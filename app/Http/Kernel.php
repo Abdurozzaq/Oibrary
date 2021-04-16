bc8053c0a1bda5e99f6ee50b5ba3e8a3c57a8162
Abdurozzaq Nurul Hadi <Abdurozzaq@outlook.com>
2021-04-16 11:39:26 +0700

    Increase API rate limiter

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:192,1',
+            'throttle:1000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
